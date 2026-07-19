6228e12413dcf3adf464bd2078d4d380d7cb121e
Mohammad Sidique Memon <siddiqememon@gmail.com>
2026-07-19 18:51:16 +0530

    Automatic LPG Gas Detection & Regulator ON/OFF system project added

---
+++ Automatic LPG Gas Detection & Regulator ON_OFF System/Code/Gas_Sensor__Humidity__temp__LCD__LED/Gas_Sensor__Humidity__temp__LCD__LED.ino
@@ -0,0 +1,103 @@
+#include <LiquidCrystal.h>
+#include <dht.h>
+
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
+
+#define lpg_sensor A1
+#define ledPin 13
+#define reLay 10
+#define dht_apin A0 // Analog Pin sensor is connected to
+dht DHT;
+
+
+
+void setup() {
+  
+
+ 
+  pinMode(lpg_sensor, INPUT);
+  pinMode(ledPin, OUTPUT);
+  pinMode(reLay,OUTPUT);
+  lcd.begin(16, 2);
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("LPG Gas Detector");
+   lcd.setCursor(1,1);
+  lcd.print("E&TC  TE- C4");
+  delay(5000);
+  lcd.clear();
+
+}
+
+void loop() {
+  
+  DHT.read11(dht_apin);
+  
+    
+    delay(50);//Wait 5 seconds before accessing sensor again.
+
+    if(digitalRead(lpg_sensor))
+  {
+//    for(DHT.temperature=0;DHT.temperature;DHT.temperature++)
+//    {
+      
+    digitalWrite(ledPin, HIGH);
+    digitalWrite(reLay, LOW);
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print("LPG Gas Leakage");
+    lcd.setCursor(4, 1);
+    lcd.print("     Alert     ");
+//    digitalWrite(ledPin, HIGH);
+//    digitalWrite(reLay, LOW);
+    delay(500);
+//    }
+//     for(DHT.temperature=25;DHT.temperature;DHT.temperature++)
+//     {
+//      lcd.clear();
+//    lcd.setCursor(5,0);
+//    lcd.print("   Fire ");
+//    lcd.setCursor(4, 1);
+//    lcd.print("  Warning  ");
+//    digitalWrite(ledPin, LOW);
+//    delay(50);
+//    digitalWrite(ledPin, HIGH);
+//    delay(50);
+//    digitalWrite(ledPin, LOW);
+//    delay(50);
+//    digitalWrite(ledPin, HIGH);
+//    delay(50);
+//      
+//     }
+    
+  }
+    
+  else 
+  {
+    digitalWrite(ledPin, LOW);
+    digitalWrite(reLay, HIGH);
+    lcd.clear();
+    lcd.setCursor(2,0);
+    lcd.print("No Leakage ");
+    delay(3000);
+    
+    lcd.clear();
+    lcd.setCursor(1,0);
+    lcd.print("Temp = ");
+  lcd.print(DHT.temperature);  //printing temperarture to the LCD display
+
+
+  lcd.print("'C");
+    lcd.setCursor(0,1);
+    lcd.print("Humi. = ");
+    lcd.print(DHT.humidity);  //printing humidity to the LCD display
+  lcd.print(" %");
+    delay(3000);
+  }
+  
+  
+  
+ 
+
+}
